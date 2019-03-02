0ff000a51525b151494681781d0156d628d4612b
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-03-02 06:48:51 -0600

    a ton of stuff

--- login.php
+++ login.php
@@ -2,16 +2,16 @@
     $db = mysqli_connect("localhost", "root", "", "authentication");
     session_start();
     <form action="register.php" method="post">
-    $username = mysql_real_escape_string($_POST['userName']);
-    $password = mysql_real_escape_string($_POST['psw']);
+    $userName = mysql_real_escape_string($_POST['userName']);
+    $psw = mysql_real_escape_string($_POST['psw']);
     
-    $password = md5($password);
-    $sql = "SELECT FROM users WHERE username = '$username' AND password = '$password'";
+    $psw = md5($psw);
+    $sql = "SELECT FROM users WHERE userName = '$userName' AND psw = '$psw'";
     $result = mysqli_query($db, $sql);
 
     if (mysqli_num_rows($result) == 1) {
         $_session['message'] = "You are now logged in";
-        $_session['username'] = $username;
+        $_session['userName'] = $userName;
         header["location; index.html"]
     } else {
         $_session['message'] = "Username/password combination incorrect";
