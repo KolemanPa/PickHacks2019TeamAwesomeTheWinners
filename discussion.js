72d6aae96992b483dc57bb5e2b4addf6bfd69bb9
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-03-03 06:12:42 -0600

    commit

--- discussion.js
+++ discussion.js
@@ -1,6 +1,6 @@
 function sportPost(sport){
     var xmlhttp = new XMLHttpRequest();
-    var url = "https://www.planagame.com/discussion.php?sport="+sport;        // either https://www.dns or localhost:2020
+    var url = "https://www.planagame.com/post.php?sport="+sport;        // either https://www.dns or localhost:2020
     xmlhttp.onreadystatechange = function(){
         if(this.readyState === 4 && this.status === 200)
         {
--- post.php
+++ post.php
@@ -4,7 +4,12 @@ ini_set('display_errors',1);
     $username = mysqli_real_escape_string($db,$_POST["userName"]);
     $txt = mysqli_rea_escape_string($db,$_POST["message"]);
     date_default_timezone_set()
-    $postdate = date(now);
     $sql = "INSERT INTO posts (userName, message, timedate, zip) VALUES ($username, $txt, NOW(), zip)";
     $result = mysqli_query($db, $sql);
+
+    while($data = mysqli_fetch_assoc($result)) {
+        foreach($data as $key => $value) {
+          echo "<$key>$value</$key>";
+        }
+      }
 ?>
