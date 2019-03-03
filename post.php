e97b15662a7682c46e78b3051e5978ba8329be31
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-03-03 05:58:12 -0600

    Create post.php

---
+++ post.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors',1);
+    $db = mysqli_connect("127.0.1","root","PuFXrzGwUE5paP", "Planagame.com");
+    $username = mysqli_real_escape_string($db,$_POST["userName"]);
+    $txt = mysqli_rea_escape_string($db,$_POST["message"]);
+    date_default_timezone_set()
+    $postdate = date(now);
+    sql = "INSERT INTO posts (userName, message, timedate, zip) VALUES ($username, $txt, NOW(), zip)";
+
+?>
