aaf2e62e5f828f2563ca8d31e08fc082c5b13a65
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-03-03 00:41:41 -0600

    fdas

---
+++ discussion.php
@@ -0,0 +1,3 @@
+<?php
+$sport = $_GET["sport"];
+?>
--- login.php
+++ login.php
@@ -7,7 +7,8 @@ if (!empty($userName) || !empty($psw) || !empty($birthday) || !empty($email)){
     $host = "localhost";
     $dbUsername = "userName";
     $dbPassword = "psw";
-    $conn = mysqli_connect($userName,$psw,$email);
+    $conn= mysqli_connect("127.0.0.1", "root", "my_password", "users");
+    $conn->query("my")
 }
 if(!$conn){
     die("connection Failed" .mysqli_connect_error());
